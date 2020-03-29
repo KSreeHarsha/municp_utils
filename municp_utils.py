7723bfb2acb0c9f5df2b37a1dc788e7f8b191c40
Harsha <harsha@Harshas-MacBook-Pro.local>
2020-03-29 20:58:10 +0530

    Entry point

--- municp_utils.py
+++ municp_utils.py
@@ -16,4 +16,20 @@ import optparse
 
 
 
+if __name__=='__main__':
 
+         # Parse command line arguments and options.
+    usage = 'usage: %prog [options]'
+    description = 'Download municipality records.'
+    p = optparse.OptionParser(usage=usage, description=description)
+    p.add_option('-f','--first_page', action='store', dest='first_page',
+                 type='int',help='First Page to download')
+    p.add_option('-l','--last_page', action='store', dest='last_page',
+                 type='int', help='Last Page to download')
+    p.set_defaults(first_page=1, last_page=1)
+
+    (opts, args) = p.parse_args()
+    first_page=opts.first_page
+    last_page=opts.last_page
+
+    print "Downloading pages from ",first_page," to ",last_page
