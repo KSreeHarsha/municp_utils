1ea2f500ad3a46856baad044c7dbeb84a1118726
Harsha <harsha@Harshas-MacBook-Pro.local>
2020-03-29 20:48:36 +0530

    Added imports

---
+++ municp_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+  
+"""
+@package css
+@file css/municp_utils.py
+@author K Sree Harsha
+@brief Utility module for retrieving congress.gov data.
+"""
+
+from mechanize import Browser
+from bs4 import BeautifulSoup
+import re
+import urllib
+import csv
+import optparse
+
+
+
+
